3d484dc1f484e6070171acfd4f3760700e35532f
Bing <Bing@xobo.org>
2018-12-28 14:41:23 +0800

    修正组件权限问题

--- panda-parent/panda-security-ui/src/main/java/org/malagu/panda/security/ui/builder/AbstractBuilder.java
+++ panda-parent/panda-security-ui/src/main/java/org/malagu/panda/security/ui/builder/AbstractBuilder.java
@@ -1,111 +1,121 @@
 package org.malagu.panda.security.ui.builder;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-
+import javax.annotation.PostConstruct;
 import org.apache.commons.lang.StringUtils;
 import org.malagu.panda.security.ui.utils.ClassUtils;
 import org.malagu.panda.security.ui.utils.ControlUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-
+import org.springframework.context.ApplicationContext;
 import com.bstek.dorado.view.AbstractViewElement;
 import com.bstek.dorado.view.widget.Component;
 
 /**
  * @author Kevin.yang
  */
-@SuppressWarnings({ "rawtypes", "unchecked" })
+@SuppressWarnings({"rawtypes", "unchecked"})
 public abstract class AbstractBuilder<T extends AbstractViewElement> implements Builder<T> {
 
-	@Autowired
-	protected Collection<Builder> builders;
+  @Autowired
+  protected Collection<Builder> builders;
+  
+  @Autowired
+  private ApplicationContext applicationContext;
+  
+  @PostConstruct
+  private void init() {
+    builders = new ArrayList<>(builders);
+    builders.add(applicationContext.getBean(this.getClass()));
+  }
 
-	@Autowired
-	private DefaultBuilder defaultBuilder;
+  @Autowired
+  private DefaultBuilder defaultBuilder;
 
-	@Value("${panda.componentPermissionFlat}")
-	private boolean componentPermissionFlat;
+  @Value("${panda.componentPermissionFlat}")
+  private boolean componentPermissionFlat;
 
-	@Override
-	public void build(T control, ViewComponent parent, ViewComponent root) {
-		if (ControlUtils.isNoSecurtiy(control)) {
-			return;
-		}
-		ViewComponent p = null;
-		if (ControlUtils.supportControlType(control)) {
-			ViewComponent component = new ViewComponent();
-			component.setId(getId(control));
-			component.setDesc(getDesc(control));
-			component.setIcon(getIcon(control));
-			component.setEnabled(isEnabled(control));
-			component.setName(getName(control));
-			if (componentPermissionFlat) {
-				root.addChildren(component);
-			} else {
-				parent.addChildren(component);
-			}
-			p = component;
-		} else {
-			p = parent;
-		}
+  @Override
+  public void build(T control, ViewComponent parent, ViewComponent root) {
+    if (ControlUtils.isNoSecurtiy(control)) {
+      return;
+    }
+    ViewComponent p = null;
+    if (ControlUtils.supportControlType(control)) {
+      ViewComponent component = new ViewComponent();
+      component.setId(getId(control));
+      component.setDesc(getDesc(control));
+      component.setIcon(getIcon(control));
+      component.setEnabled(isEnabled(control));
+      component.setName(getName(control));
+      if (componentPermissionFlat) {
+        root.addChildren(component);
+      } else {
+        parent.addChildren(component);
+      }
+      p = component;
+    } else {
+      p = parent;
+    }
 
-		buildChildren(control, p, root);
-	}
+    buildChildren(control, p, root);
+  }
 
-	protected void buildChildren(T control, ViewComponent parent, ViewComponent root) {
-		Collection children = getChildren(control);
-		if (children != null) {
-			for (Object child : children) {
-				if (child instanceof AbstractViewElement) {
-					boolean exist = false;
-					AbstractViewElement c = (AbstractViewElement) child;
-					for (Builder builder : builders) {
-						if (builder.support(c)) {
-							exist = true;
-							builder.build(c, parent, root);
-							break;
-						}
-					}
-					if (!exist) {
-						defaultBuilder.build(c, parent, root);
-					}
-				}
-			}
-		}
-	}
+  protected void buildChildren(T control, ViewComponent parent, ViewComponent root) {
+    Collection children = getChildren(control);
+    if (children != null) {
+      for (Object child : children) {
+        if (child instanceof AbstractViewElement) {
+          boolean exist = false;
+          AbstractViewElement c = (AbstractViewElement) child;
+          for (Builder builder : builders) {
+            if (builder.support(c)) {
+              exist = true;
+              builder.build(c, parent, root);
+              break;
+            }
+          }
+          if (!exist) {
+            defaultBuilder.build(c, parent, root);
+          }
+        }
+      }
+    }
+  }
 
-	protected String getId(T control) {
-		if (control instanceof Component) {
-			return ((Component) control).getId();
-		}
-		return null;
-	}
+  protected String getId(T control) {
+    if (control instanceof Component) {
+      return ((Component) control).getId();
+    }
+    return null;
+  }
 
-	protected String getIcon(T control) {
-		return ">dorado/res/" + control.getClass().getName().replaceAll("\\.", "/") + ".png";
-	}
+  protected String getIcon(T control) {
+    return ">dorado/res/" + control.getClass().getName().replaceAll("\\.", "/") + ".png";
+  }
 
-	protected String getName(T control) {
-		return control.getClass().getSimpleName();
-	}
+  protected String getName(T control) {
+    return control.getClass().getSimpleName();
+  }
 
-	protected boolean isEnabled(T control) {
-		return StringUtils.isNotEmpty(getId(control));
-	}
+  protected boolean isEnabled(T control) {
+    return StringUtils.isNotEmpty(getId(control));
+  }
 
-	protected Collection getChildren(T control) {
-		return Collections.EMPTY_LIST;
-	}
+  protected Collection getChildren(T control) {
+    return Collections.EMPTY_LIST;
+  }
 
-	protected String getDesc(T control) {
-		return ControlUtils.getSecurityDesc(control);
-	}
+  protected String getDesc(T control) {
+    return ControlUtils.getSecurityDesc(control);
+  }
 
-	@Override
-	public boolean support(Object control) {
-		Class<?> clazz = ClassUtils.getGenericType(this.getClass(), 0);
-		return clazz.isAssignableFrom(control.getClass());
-	}
+  @Override
+  public boolean support(Object control) {
+    Class<?> clazz = ClassUtils.getGenericType(this.getClass(), 0);
+    return clazz.isAssignableFrom(control.getClass());
+  }
 
 }
--- panda-parent/panda-security/src/main/java/org/malagu/panda/security/service/ComponentServiceImpl.java
+++ panda-parent/panda-security/src/main/java/org/malagu/panda/security/service/ComponentServiceImpl.java
@@ -3,7 +3,6 @@ package org.malagu.panda.security.service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-
 import org.malagu.linq.JpaUtil;
 import org.malagu.panda.security.orm.Component;
 import org.malagu.panda.security.orm.Permission;
@@ -19,24 +18,28 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class ComponentServiceImpl implements ComponentService {
 
-	@Override
-	public List<Component> findAll() {
-		List<Component> components = JpaUtil.linq(Component.class).list();
-		List<Permission> permissions = JpaUtil.linq(Permission.class).equal("resourceType", Component.RESOURCE_TYPE).list();
-		if (!permissions.isEmpty()) {
-			Map<String, Component> componentMap = JpaUtil.index(components);
-			for (Permission permission : permissions) {
-				Component component = componentMap.get(permission.getResourceId());
-				List<ConfigAttribute> configAttributes = component.getAttributes();
-				if (configAttributes == null) {
-					configAttributes = new ArrayList<ConfigAttribute>();
-					component.setAttributes(configAttributes);
-				}
-				configAttributes.add(permission);
-			}
-		}
-		return components;
+  @Override
+  public List<Component> findAll() {
+    List<Component> components = JpaUtil.linq(Component.class).list();
+    List<Permission> permissions =
+        JpaUtil.linq(Permission.class).equal("resourceType", Component.RESOURCE_TYPE).list();
+    if (!permissions.isEmpty()) {
+      Map<String, Component> componentMap = JpaUtil.index(components);
+      for (Permission permission : permissions) {
+        Component component = componentMap.get(permission.getResourceId());
+        if (component == null) {
+          continue;
+        }
+        List<ConfigAttribute> configAttributes = component.getAttributes();
+        if (configAttributes == null) {
+          configAttributes = new ArrayList<ConfigAttribute>();
+          component.setAttributes(configAttributes);
+        }
+        configAttributes.add(permission);
+      }
+    }
+    return components;
 
-	}
+  }
 
 }
