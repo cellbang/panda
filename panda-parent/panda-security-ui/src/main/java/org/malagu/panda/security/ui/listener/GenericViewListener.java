17bedbbf52d688ae03851ad7be55bace6a5a8677
Bing <Bing@xobo.org>
2019-01-07 14:42:51 +0800

    允许自定义 view title

--- panda-parent/panda-security-ui/src/main/java/org/malagu/panda/security/ui/listener/GenericViewListener.java
+++ panda-parent/panda-security-ui/src/main/java/org/malagu/panda/security/ui/listener/GenericViewListener.java
@@ -6,32 +6,36 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-
+import org.springframework.util.StringUtils;
 import com.bstek.dorado.data.listener.GenericObjectListener;
 import com.bstek.dorado.view.View;
 
 @Component
 public class GenericViewListener extends GenericObjectListener<View> {
 
-	@Value("${panda.appName}")
-	private String applicationTitle;
-	
-	@Autowired
-	private ViewFilter viewFilter;
-	
-	@Override
-	public boolean beforeInit(View view) throws Exception {
-		return true;
-	}
+  @Value("${panda.appName}")
+  private String applicationTitle;
 
-	@Override
-	public void onInit(View view) throws Exception {
-		view.setTitle(applicationTitle);
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		if (auth.isAuthenticated() && auth.getPrincipal() instanceof String && "anonymousUser".equals(auth.getPrincipal())) {
-			return;
-		}
-		viewFilter.invoke(view);
-	}
+  @Autowired
+  private ViewFilter viewFilter;
+
+  @Override
+  public boolean beforeInit(View view) throws Exception {
+    return true;
+  }
+
+  @Override
+  public void onInit(View view) throws Exception {
+    String title = view.getTitle();
+    if (StringUtils.isEmpty(title)) {
+      view.setTitle(applicationTitle);
+    }
+    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    if (auth.isAuthenticated() && auth.getPrincipal() instanceof String
+        && "anonymousUser".equals(auth.getPrincipal())) {
+      return;
+    }
+    viewFilter.invoke(view);
+  }
 
 }
